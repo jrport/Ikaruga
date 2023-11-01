11e842858f7457fd00cc455b201b6a1cbf69410a
jrport <joao6roberto@gmail.com>
2023-11-01 18:16:41 -0300

    menu and background kinda fixed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from tupy import *
+from tupy import run
 from menu import *
 
 def main():
+    backgroung = Image("./assets/backgroung_jpg",100,100)
     menu = Menu()
-
+    
 if __name__ == "__main__":
     main()
-
+    
 run(globals())
--- menu.py
+++ menu.py
@@ -1,52 +1,11 @@
+from tupy import BaseGroup
 from tupy import Label
 from tupy import Image
+from tupy import Rectangle
 
-class option(Label):
-    def __init__(self, name: str, x: int, y: int) -> None:
-        super().__init__(name, x, y)
-        self.selected: bool = False
-    
-    def update(self) -> None:
-        if self.selected:
-            self.font = "Arial 28"
-            self.color = "red"
-            return
-        self.font = "Arial 20"
-        self.color = "black"
-
-class Menu(Image):
+class background(Image):
     def __init__(self) -> None:
-        super().__init__("./assets/backgroung_01.jpg")
-        self.cur_option: int = 1
-        self.items: list[option] = [
-                option("Iniciar", 225, 300), \
-                option("Dificuldade", 225, 275), \
-                option("Sair", 225, 250) \
-                ]
-    
-    def update(self) -> None:
-        if keyboard.is_key_just_down("Down"):
-            self.change_select(1)
-        if keyboard.is_key_just_down("Up"):
-            self.change_select(-1)
-
-    def change_select(self, jmp: int) -> None:
-        if (self.cur_option + jmp) > 3:
-            self.cur_option = 1
-            return
-        if (self.cur_option + jmp) < 1:
-            self.cur_option = 3
-            return
-        self.cur_option += jmp
-    
-    def enter(self):
-        pass
-
-    def change_dificulty(self):
-        pass
-
-    def exit(self):
-        pass
-
-
+        super().__init__('./background_01.jpg', 100, 100)
         
+class menu(BaseGroup):
+    
